53e01367495d7fe78a48547e37090563dd2f0dc6
Jashon Osala <jashonosala@gmail.com>
2022-08-31 12:54:09 +0300

    Changes

---
+++ prompt.h
@@ -0,0 +1,28 @@
+#ifndef _PROMPT_F_H
+#define _PROMPT_F_H
+
+#include "root.h"
+
+/**
+ * ctrl_c - ignore ctrl-c and reprompt
+ * @n: takes int from signal
+ */
+__home__ void ctrl_c(int n)
+{
+	(void) n;
+	write(STDOUT_FILENO, "\n$ ", 3);
+}
+
+/**
+ * prompt - prompt
+ * @env: env
+ *
+ * Return: int
+ */
+__home__ int prompt(char **env)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	return (0);
+}
--- root.h
+++ root.h
@@ -14,7 +14,15 @@
  */
 
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 #endif
