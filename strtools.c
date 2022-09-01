1c452980a8d409fcd52e3ec72bdf05cf32d1102e
Arlight16 <arlightglo@gmail.com>
2022-08-31 22:01:13 -0700

    comparing strings with length

--- strtools.c
+++ strtools.c
@@ -1,13 +1,13 @@
 #include "root.h"
 
 /**
- * hsh_strlen - len of str
+ * _strlen - len of str
  * @s: string
  *
  * Return: len of str
  */
 
-int hsh_strlen(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
@@ -17,4 +17,27 @@ int hsh_strlen(char *s)
 	}
 
 	return (i);
+}
+
+/**
+ * hsh_strncmp - Compare two strings with a specific length
+ * @s1: String 1
+ * @s2: String 2
+ * @len: Length
+ *
+ * Return: Difference of characters (0 - Equal)
+ */
+int hsh_strncmp(char *s1, char *s2, int len)
+{
+	int i = 1;
+
+	while (i <= len)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+		i++;
+	}
+	return (0);
 }
